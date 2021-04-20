c3e7601136b16f68103a7a416ac13566fb793904
Kaio Oliveira <kaiohenrique2601@gmail.com>
2021-04-20 16:06:52 -0300

    swagger

--- super_heroes/urls.py
+++ super_heroes/urls.py
@@ -1,12 +1,34 @@
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Super Heroes API",
+      default_version='v1',
+      description="An api for Heroes",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="kaiohenrique2601@gmail.com"),
+      license=openapi.License(name="Test License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('character/',include('character.urls')),
     path('user/',include('user.urls')),
     path('login/', obtain_jwt_token),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('documentation/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
